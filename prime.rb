896ed2869a25d9ac0d844b687f9e4cd75e304e27
Mark A Guza <guzamaguza@gmail.com>
2020-01-22 06:29:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 
 def prime?(num)
   #zero2nine = [2, 3, 4, 5, 6, 7, 8, 9,]
-  zero2nine = [2..100]
+  zero2nine = [2...100]
   if num <= 1
       return false
   elsif num == 2 || num == 3 || num == 5 || num == 7
