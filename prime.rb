fb41cced5db4aff68ce4ff3a462f77c94694b8f7
Mark A Guza <guzamaguza@gmail.com>
2020-01-22 06:02:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,17 +2,19 @@
 
 def prime?(num)
   zero2nine = [2, 3, 4, 5, 6, 7, 8, 9]
+  if num <= 1
+      return false
+  elsif num == 2 || num == 3 || num == 5 || num == 7
+      return true
+  elsif num == 4 || num == 6 || num == 8 || num == 9
+      return false
+  end
+  
   zero2nine.each do |int|
-    if num <= 1
-        return false
-    elsif num == 2 || num == 3 || num == 5 || num == 7
-        return true
-    elsif num == 4 || num == 6 || num == 8 || num == 9
-        return false
-    elsif (num%int) == 0
-        return false
-    else
-        return true
-    end
+  if num%int == 0
+      return false
+  else
+      return true
+  end
   end
 end
