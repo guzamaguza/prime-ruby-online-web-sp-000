d317316bb0669a456e0ca06e7ebf2be31e55eea8
Mark A Guza <guzamaguza@gmail.com>
2020-01-22 05:44:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(num)
   zero2nine = [2, 3, 4, 5, 6, 7, 8, 9]
   zero2nine.each do |int|
     if num <= 1
-      return false
+        return false
     elsif num % int == 0
         return false
     else
